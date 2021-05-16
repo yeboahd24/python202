0b33efddf9f62d787e74fd650453543d05b21f42
Yeboah Dominic <yeboahd24@gmail.com>
2021-05-15 21:06:24 -0700

    slugfield modify to prepopulated_fields

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Category, Product
-
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product)
